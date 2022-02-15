702243f53f4b4ecc841f58cc8cad13584d5582e5
Miriam <miridogsteam@gmail.com>
2022-02-15 10:07:24 +0100

    Slider configuracion volumen
    - Funciona cambio volumen entre escenas de efectos y música

--- PR_ZAXXON_RomeroMiriam/Assets/Scripts/UI/ConfigScript.cs
+++ PR_ZAXXON_RomeroMiriam/Assets/Scripts/UI/ConfigScript.cs
@@ -8,56 +8,27 @@ using UnityEngine.Audio;
 public class ConfigScript : MonoBehaviour
 {
     //VARIABLES SLIDER MUSICA Y EFECTOS
-
-   // [SerializeField] Slider MusicVolume;
-
-    //float volumen;
-
-   // [SerializeField] Slider FxVolume;
-
-    //float volumenfx;
+    public Slider bgmSlider, sfxSlider;
+    public AudioMixer mixer;
 
     
-
-    public AudioMixer mixer;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
 
-        /*volMusicaSL.value = GameManager.VolumenMusica;
-        volEfectosSL.value = GameManager.VolumenEfectos;*/
+        bgmSlider.onValueChanged.AddListener(delegate {SetSound("BGMVolume", bgmSlider.value); });
+        sfxSlider.onValueChanged.AddListener(delegate {SetSound("SFXVolume", sfxSlider.value); });
        
-
     }
 
-
-
-    // Update is called once per frame
-    void Update()
+    public void SetSound(string param, float soundLevel)
     {
-        
+        mixer.SetFloat(param, soundLevel);
     }
 
-    public void SetMusic(float soundLevelMusic)
-    {
-        mixer.SetFloat("ExposedMusica", soundLevelMusic);
-    }
-
-    /*public void SetFX(float soundLevelFX)
-    {
-        mixer.SetFloat("ExposedEfectos", soundLevelFX);
-    }*/
     
-
-    /*public void CambiarVolumen()
-    {
-        volumen = volMusicaSL.value;
-        volumenfx = volEfectosSL.value;
-        
-    }*/
     public void MenuInicio()
     {
         SceneManager.LoadScene(0);
